9d08d0034298e562ee28a29355f206fa82022cea
Mike Chen <stupidchen@foxmail.com>
2018-02-28 23:51:36 +0800

    Finished: Add UserController and the stubs in DataFactory.

---
+++ src/main/java/com/stupidchen/blog/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.stupidchen.blog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mike on 2/28/18.
+ */
+
+@RestController
+public class UserController {
+}
--- src/main/java/com/stupidchen/blog/data/DataFactory.java
+++ src/main/java/com/stupidchen/blog/data/DataFactory.java
@@ -2,17 +2,44 @@ package com.stupidchen.blog.data;
 
 import com.stupidchen.blog.model.Archive;
 import com.stupidchen.blog.model.Response;
+import com.stupidchen.blog.model.User;
 
 /**
  * Created by mike on 12/15/17.
  */
 public class DataFactory {
-    public static Archive getArchive(String id) {
+    public static Archive[] getArchives() {
         return null;
     }
 
-    public static Response putArchive(String id, Archive archive) {
-
+    public static Response addArchive(Archive archive) {
         return null;
     }
+
+    public static Response updateArchive(Archive archive) {
+        return null;
+    }
+
+    public static Archive getArchiveById(String id) {
+        return null;
+    }
+
+    public static Response updateArchiveWithId(String id, Archive archive) {
+        return null;
+    }
+
+    public static Response deleteArchive(String id) {
+        return null;
+    }
+
+
+    public static Response createUser(User user) {
+        return null;
+    }
+
+    public static Response loginUser(String username, String password) {
+        return null;
+    }
+
+
 }
